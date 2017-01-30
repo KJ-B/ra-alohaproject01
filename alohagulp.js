71941ec0f97317a9c07e6648f3104ad91ef8482b
KJ-B <donkjb@Kevins-MacBook-Pro.local>
2017-01-30 15:59:46 -0500

    Lots....

---
+++ alohagulp.js
@@ -0,0 +1,35 @@
+var rename = require("gulp-rename");
+var gulp = require("gulp");
+var uglifycss = require("gulp-uglifycss");
+var concat = require("gulp-concat");
+ 
+gulp
+gulp.task("aloha",function (){
+	// what files do i want to look for
+	gulp.src("raacademyproject01/css/*.css")
+
+	// rename the css file(s) i found
+	.pipe(concat("all.css"))
+	.pipe(rename("theme.css"))
+	// take the ive just renamed and minify them
+	.pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
+	// where should i put the renamed file(s)
+	.pipe(gulp.dest("./dist/"));
+
+
+	
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('raacademyproject01/css/*.css', ['aloha']);
+});
+
+	// Default Task
+
+gulp.task("kitty", function () {
+		// body...
+})
